1f48198a810c7b4dff78d869eebf7dc9797c1199
qwuko <verast2006@gmail.com>
2026-03-12 14:30:05 +0300

    Создание класса MusicTrack

---
+++ MusicCollectionApp/MusicCollectionApp/MusicTrack.cs
@@ -0,0 +1,20 @@
+﻿public class MusicTrack
+{
+    public string Artist { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+
+    public MusicTrack(string artist, string title, string genre, int year)
+    {
+        Artist = artist;
+        Title = title;
+        Genre = genre;
+        Year = year;
+    }
+
+    public override string ToString()
+    {
+        return $"{Artist} - {Title} ({Year}) [{Genre}]";
+    }
+}
