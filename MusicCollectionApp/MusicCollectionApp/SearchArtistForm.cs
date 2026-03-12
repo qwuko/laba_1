64f85e6decddcced40c71f555de2357bfa83116c
qwuko <verast2006@gmail.com>
2026-03-12 14:38:47 +0300

    Создание графического интерфейса (Форма SearchArtistForm)

---
+++ MusicCollectionApp/MusicCollectionApp/SearchArtistForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MusicCollectionApp
+{
+    public partial class SearchArtistForm : Form
+    {
+        public string Artist { get; set; }
+
+        public SearchArtistForm()
+        {
+            InitializeComponent();
+            AttachEvents();
+        }
+
+        private void AttachEvents()
+        {
+            okButton.Click += OkButton_Click;
+            cancelButton.Click += CancelButton_Click;
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            Artist = artistTextBox.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
